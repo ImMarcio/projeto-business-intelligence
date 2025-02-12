b43aed58c917b4a037e60ef995e4e85219fde4da
Márcio J. Silva <105254896+ImMarcio@users.noreply.github.com>
2025-02-11 21:56:52 -0300

    feat: novo script para criar o banco de dados

--- Scripts/create-tables.sql
+++ Scripts/create-tables.sql
@@ -42,27 +42,10 @@ CREATE TABLE public.Clientes (
 
 ALTER SEQUENCE public.clientes_sk_cliente_seq_1 OWNED BY public.Clientes.SK_cliente;
 
-CREATE SEQUENCE public.fornecedores_sk_fornecedor_seq;
-
-CREATE TABLE public.Fornecedores (
-                SK_fornecedor INTEGER NOT NULL DEFAULT nextval('public.fornecedores_sk_fornecedor_seq'),
-                CodigoDoFornecedor INTEGER NOT NULL,
-                NomeDaEmpresa CHAR(40),
-                NomeDoContato CHAR(30),
-                CONSTRAINT forpk PRIMARY KEY (SK_fornecedor)
-);
-
-
-ALTER SEQUENCE public.fornecedores_sk_fornecedor_seq OWNED BY public.Fornecedores.SK_fornecedor;
-
-CREATE UNIQUE INDEX fornecedores_idx
- ON public.Fornecedores
- ( CodigoDoFornecedor );
-
-CREATE SEQUENCE public.transportadoras_sk_transportadora_seq;
+CREATE SEQUENCE public.transportadoras_sk_transportadora_seq_1;
 
 CREATE TABLE public.Transportadoras (
-                SK_transportadora VARCHAR NOT NULL DEFAULT nextval('public.transportadoras_sk_transportadora_seq'),
+                SK_transportadora VARCHAR NOT NULL DEFAULT nextval('public.transportadoras_sk_transportadora_seq_1'),
                 CodigoDaTransportadora INTEGER NOT NULL,
                 NomeDaEmpresa CHAR(40),
                 Telefone CHAR(24),
@@ -70,65 +53,22 @@ CREATE TABLE public.Transportadoras (
 );
 
 
-ALTER SEQUENCE public.transportadoras_sk_transportadora_seq OWNED BY public.Transportadoras.SK_transportadora;
+ALTER SEQUENCE public.transportadoras_sk_transportadora_seq_1 OWNED BY public.Transportadoras.SK_transportadora;
 
 CREATE UNIQUE INDEX transportadoras_idx
  ON public.Transportadoras
  ( CodigoDaTransportadora );
 
-CREATE SEQUENCE public.pedidos_sk_pedido_seq;
-
-CREATE SEQUENCE public.pedidos_numerodopedido_seq;
-
-CREATE TABLE public.Pedidos (
-                SK_pedido INTEGER NOT NULL DEFAULT nextval('public.pedidos_sk_pedido_seq'),
-                NumeroDoPedido INTEGER NOT NULL DEFAULT nextval('public.pedidos_numerodopedido_seq'),
-                SK_transportadora VARCHAR NOT NULL,
-                SK_TempoEnvio INTEGER NOT NULL,
-                SK_TempoPedido INTEGER NOT NULL,
-                DataDeEntrega DATE NOT NULL,
-                DataDeEnvio DATE,
-                DataDoPedido DATE NOT NULL,
-                SK_TempoEntrega INTEGER NOT NULL,
-                CONSTRAINT sk_pedido PRIMARY KEY (SK_pedido)
-);
-
-
-ALTER SEQUENCE public.pedidos_sk_pedido_seq OWNED BY public.Pedidos.SK_pedido;
-
-ALTER SEQUENCE public.pedidos_numerodopedido_seq OWNED BY public.Pedidos.NumeroDoPedido;
-
-CREATE UNIQUE INDEX dim_pedido_idx
- ON public.Pedidos
- ( NumeroDoPedido );
-
-CREATE SEQUENCE public.categorias_sk_categoria_seq;
-
-CREATE TABLE public.Categorias (
-                SK_categoria INTEGER NOT NULL DEFAULT nextval('public.categorias_sk_categoria_seq'),
-                CodigoDaCategoria INTEGER NOT NULL,
-                NomeDaCategoria CHAR(15),
-                Descricao CHAR(300),
-                CONSTRAINT catpk PRIMARY KEY (SK_categoria)
-);
-
-
-ALTER SEQUENCE public.categorias_sk_categoria_seq OWNED BY public.Categorias.SK_categoria;
-
-CREATE UNIQUE INDEX categorias_idx
- ON public.Categorias
- ( CodigoDaCategoria );
-
 CREATE SEQUENCE public.produtos_sk_produto_seq;
 
 CREATE TABLE public.Produtos (
                 SK_Produto INTEGER NOT NULL DEFAULT nextval('public.produtos_sk_produto_seq'),
+                EmpresaFornecedora CHAR(40),
                 CodigoDoProduto INTEGER NOT NULL,
                 NomeDoProduto CHAR(40),
                 QuantidadePorUnidade CHAR(25),
                 PrecoUnitario NUMERIC(10,2),
-                SK_fornecedor INTEGER NOT NULL,
-                SK_categoria INTEGER NOT NULL,
+                Categoria VARCHAR NOT NULL,
                 CONSTRAINT prodpk PRIMARY KEY (SK_Produto)
 );
 
@@ -160,54 +100,40 @@ CREATE UNIQUE INDEX funcionarios_idx
  ON public.Funcionarios
  ( CodigoDoFuncionario );
 
-CREATE SEQUENCE public.fatos_vendas_sk_venda_seq;
-
 CREATE TABLE public.Fatos_Vendas (
-                SK_venda INTEGER NOT NULL DEFAULT nextval('public.fatos_vendas_sk_venda_seq'),
-                NumeroDaVenda INTEGER NOT NULL,
+                PrecoTotal VARCHAR NOT NULL,
                 PrecoUnitario NUMERIC(14,2),
+                Frete NUMERIC(14,2),
                 Quantidade SMALLINT,
                 Desconto NUMERIC(14,2),
                 PaisDeDestino CHAR(15),
-                COD_produto INTEGER NOT NULL,
-                SK_Pedido INTEGER NOT NULL,
-                SK_Tempo INTEGER NOT NULL,
+                SK_produto INTEGER NOT NULL,
                 SK_Cliente INTEGER NOT NULL,
                 SK_Funcionario INTEGER NOT NULL,
-                CONSTRAINT pedpk PRIMARY KEY (SK_venda)
+                SK_transportadora VARCHAR NOT NULL,
+                SK_TempoPedido INTEGER NOT NULL,
+                SK_TempoEntrega INTEGER NOT NULL,
+                SK_TempoEnvio INTEGER NOT NULL
 );
-COMMENT ON COLUMN public.Fatos_Vendas.SK_Tempo IS 'Data de venda geral';
+COMMENT ON COLUMN public.Fatos_Vendas.SK_TempoPedido IS 'Data de venda geral';
 
 
-ALTER SEQUENCE public.fatos_vendas_sk_venda_seq OWNED BY public.Fatos_Vendas.SK_venda;
-
-CREATE UNIQUE INDEX fatos_vendas_idx
- ON public.Fatos_Vendas
- ( NumeroDaVenda );
-
-ALTER TABLE public.Pedidos ADD CONSTRAINT dim_pedido_dim_tempo_fk
-FOREIGN KEY (SK_TempoEntrega)
-REFERENCES public.Tempo (SK_Tempo)
-ON DELETE NO ACTION
-ON UPDATE NO ACTION
-NOT DEFERRABLE;
-
-ALTER TABLE public.Pedidos ADD CONSTRAINT dim_tempo_dim_pedido_fk
+ALTER TABLE public.Fatos_Vendas ADD CONSTRAINT dim_tempo_fatos_vendas_fk
 FOREIGN KEY (SK_TempoPedido)
 REFERENCES public.Tempo (SK_Tempo)
 ON DELETE NO ACTION
 ON UPDATE NO ACTION
 NOT DEFERRABLE;
 
-ALTER TABLE public.Pedidos ADD CONSTRAINT dim_tempo_dim_pedido_fk1
-FOREIGN KEY (SK_TempoEnvio)
+ALTER TABLE public.Fatos_Vendas ADD CONSTRAINT tempo_fatos_vendas_fk
+FOREIGN KEY (SK_TempoEntrega)
 REFERENCES public.Tempo (SK_Tempo)
 ON DELETE NO ACTION
 ON UPDATE NO ACTION
 NOT DEFERRABLE;
 
-ALTER TABLE public.Fatos_Vendas ADD CONSTRAINT dim_tempo_fatos_vendas_fk
-FOREIGN KEY (SK_Tempo)
+ALTER TABLE public.Fatos_Vendas ADD CONSTRAINT tempo_fatos_vendas_fk1
+FOREIGN KEY (SK_TempoEnvio)
 REFERENCES public.Tempo (SK_Tempo)
 ON DELETE NO ACTION
 ON UPDATE NO ACTION
@@ -220,36 +146,15 @@ ON DELETE NO ACTION
 ON UPDATE NO ACTION
 NOT DEFERRABLE;
 
-ALTER TABLE public.Produtos ADD CONSTRAINT prodfor
-FOREIGN KEY (SK_fornecedor)
-REFERENCES public.Fornecedores (SK_fornecedor)
-ON DELETE NO ACTION
-ON UPDATE NO ACTION
-NOT DEFERRABLE;
-
-ALTER TABLE public.Pedidos ADD CONSTRAINT transportadoras_dim_pedido_fk
+ALTER TABLE public.Fatos_Vendas ADD CONSTRAINT transportadoras_fatos_vendas_fk
 FOREIGN KEY (SK_transportadora)
 REFERENCES public.Transportadoras (SK_transportadora)
 ON DELETE NO ACTION
 ON UPDATE NO ACTION
 NOT DEFERRABLE;
 
-ALTER TABLE public.Fatos_Vendas ADD CONSTRAINT dim_pedido_fatos_vendas_fk
-FOREIGN KEY (SK_Pedido)
-REFERENCES public.Pedidos (SK_pedido)
-ON DELETE NO ACTION
-ON UPDATE NO ACTION
-NOT DEFERRABLE;
-
-ALTER TABLE public.Produtos ADD CONSTRAINT prodcat
-FOREIGN KEY (SK_categoria)
-REFERENCES public.Categorias (SK_categoria)
-ON DELETE NO ACTION
-ON UPDATE NO ACTION
-NOT DEFERRABLE;
-
 ALTER TABLE public.Fatos_Vendas ADD CONSTRAINT produtos_fatos_vendas_fk
-FOREIGN KEY (COD_produto)
+FOREIGN KEY (SK_produto)
 REFERENCES public.Produtos (SK_Produto)
 ON DELETE NO ACTION
 ON UPDATE NO ACTION
