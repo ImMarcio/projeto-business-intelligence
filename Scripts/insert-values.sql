3ae630fe3f82ddb56efd4af40a3a6a9513d30ec3
Márcio J. Silva <105254896+ImMarcio@users.noreply.github.com>
2025-02-26 09:39:53 -0300

    feat: imagens das transformations e jobs

--- Scripts/insert-values.sql
+++ Scripts/insert-values.sql
@@ -17,19 +17,19 @@
 
 
 
--- WITH Datas AS (
---     SELECT d::DATE AS data
---     FROM generate_series('1996-01-01'::date, '1998-12-31'::date, '1 day'::interval) AS t(d)
--- )
--- INSERT INTO Tempo (data, ano, trimestre, mes, dia, diadasemana, nomedomes)
--- SELECT data,
---        to_char(data, 'YYYY')::INTEGER,  -- Conversão para inteiro
---        (to_char(data, 'MM')::INTEGER - 1) / 3 + 1,
---        to_char(data, 'MM')::INTEGER,  -- Conversão para inteiro
---        to_char(data, 'DD')::INTEGER,  -- Conversão para inteiro
---        to_char(data, 'D')::INTEGER,
---        to_char(data, 'YYYY-MM')
--- FROM Datas;
+WITH Datas AS (
+    SELECT d::DATE AS data
+    FROM generate_series('1996-01-01'::date, '1998-12-31'::date, '1 day'::interval) AS t(d)
+)
+INSERT INTO Tempo (data, ano, trimestre, mes, dia, diadasemana, nomedomes)
+SELECT data,
+       to_char(data, 'YYYY')::INTEGER,  -- Conversão para inteiro
+       (to_char(data, 'MM')::INTEGER - 1) / 3 + 1,
+       to_char(data, 'MM')::INTEGER,  -- Conversão para inteiro
+       to_char(data, 'DD')::INTEGER,  -- Conversão para inteiro
+       to_char(data, 'D')::INTEGER,
+       to_char(data, 'YYYY-MM')
+FROM Datas;
 
 
 -- INSERT INTO categorias (codigodacategoria, nomedacategoria, descricao)
